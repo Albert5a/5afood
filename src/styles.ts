3fa0da536e9cee62d197c93d57183f0b420f97a1
Albert <albertsoares97@gmail.com>
2023-10-24 22:36:03 -0300

    chore: inital configs

---
+++ src/styles.ts
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const Colors = {
+  colorPrimary: '#FFEBD9',
+  colorSecondary: '#E66767',
+  yellow: '#FFB930',
+  white: '#FFFFFF',
+  black: '#000000',
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+  }
+
+  body {
+    background-color: ${Colors.white};
+    color: ${Colors.colorSecondary};
+  }
+`
