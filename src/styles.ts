36b33b7723b377ea7c375dc6788544b0b76576f9
Albert <albertsoares97@gmail.com>
2024-09-26 14:24:04 -0300

    adjust background

--- src/styles.ts
+++ src/styles.ts
@@ -2,10 +2,11 @@ import { styled, createGlobalStyle } from 'styled-components'
 
 export const Colors = {
   colorPrimary: '#FFEBD9',
+  pinkLight: '#FFF8F2',
   colorSecondary: '#E66767',
   yellow: '#FFB930',
   white: '#FFFFFF',
-  black: '#000000',
+  black: '#000000'
 }
 
 export const GlobalCss = createGlobalStyle`
@@ -23,7 +24,7 @@ export const GlobalCss = createGlobalStyle`
 `
 
 export const AppContainer = styled.div`
-  background-color: ${Colors.white};
+  background-color: ${Colors.pinkLight};
 `
 
 export const Container = styled.div`
